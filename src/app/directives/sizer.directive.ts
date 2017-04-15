bfc50d15fe732b2d8d9884d32c10f287e1427f63
aniruddha <aniruddha.kudalkar@gmail.com>
2017-04-15 15:29:46 +0530

    directives : property binding

--- src/app/directives/sizer.directive.ts
+++ src/app/directives/sizer.directive.ts
@@ -1,34 +1,31 @@
-import { Directive, ElementRef, OnInit, Renderer2, HostListener } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[sizer]'
 })
 export class SizerDirective implements OnInit {
 
+  @Input() sizer : string;
+  
   constructor(private elRef: ElementRef, private rend: Renderer2) {
   }
 
   ngOnInit() {
-    this.rend.setStyle(this.elRef.nativeElement, 'width', '200px');
+    this.rend.setStyle(this.elRef.nativeElement, 'width', this.sizer);
     // this.elRef.nativeElement.style.width = '500px';
-    console.log(`ngOnInit`);
   }
 
   @HostListener('mouseenter')
   whenMouseEnters() {
-    console.log(`mouseenter`);
-    this.rend.removeClass(this.elRef.nativeElement, 'mixMn');
-    this.rend.addClass(this.elRef.nativeElement, 'mixLg');
-    //this.rend.setStyle(this.elRef.nativeElement, 'width', '500px');
-    //this.rend.setStyle(this.elRef.nativeElement, 'background','red');
+    //this.rend.removeClass(this.elRef.nativeElement, 'mixMn');
+    //this.rend.addClass(this.elRef.nativeElement, 'mixLg');
+    this.rend.setStyle(this.elRef.nativeElement, 'width', '500px');
   }
 
   @HostListener('mouseleave')
   whenMouseLeaves() {
-    console.log(`mouseleave`);
-    this.rend.removeClass(this.elRef.nativeElement, 'mixLg');
-    this.rend.addClass(this.elRef.nativeElement, 'mixMn');
-    // this.rend.setStyle(this.elRef.nativeElement, 'width', '200px');
-    // this.rend.setStyle(this.elRef.nativeElement, 'background','white');
+    // this.rend.removeClass(this.elRef.nativeElement, 'mixLg');
+    // this.rend.addClass(this.elRef.nativeElement, 'mixMn');
+    this.rend.setStyle(this.elRef.nativeElement, 'width', this.sizer);
   }
 }
