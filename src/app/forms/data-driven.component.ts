85fb138cfd639ae98d3cc0ecd75fe1059ec972c5
aniruddha <aniruddha.kudalkar@gmail.com>
2017-04-29 11:29:27 +0530

    forms : data driven form magmyt

--- src/app/forms/data-driven.component.ts
+++ src/app/forms/data-driven.component.ts
@@ -8,12 +8,18 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
 })
 export class DataDrivenComponent implements OnInit {
 
+  cities = [
+    'Pune', 'Mumbai', 'Nagpur', 'Kolhapur'
+  ];
   myForm: FormGroup;
   constructor(formBuilder: FormBuilder) {
     this.myForm = formBuilder.group({
-      userName: formBuilder.control('', [Validators.required, this.custValidator]),
+      user: formBuilder.group({
+        userName: formBuilder.control('', [Validators.required, this.custValidator]),
+        email: formBuilder.control('', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")])
+      }),
       password: formBuilder.control('', [Validators.required]),
-      email: formBuilder.control('', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")])
+      city: formBuilder.control('')
     });
   }
 
@@ -25,7 +31,7 @@ export class DataDrivenComponent implements OnInit {
   }
 
   custValidator(fc: FormControl): { [key: string]: string } {
-    if (fc.value === 'an') return { isValid: 'failed' };
+    if (fc.value === 'an') return { isValid: 'need full user name' };
     return null;
   }
 }
